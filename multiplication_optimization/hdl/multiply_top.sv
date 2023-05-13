eb3470a985634c18fa737735fd550e8a23d6b9b5
MarcEftimie <marc.eftimie@gmail.com>
2023-05-13 02:11:28 -0700

    IEEE 754 float multiplier completed

--- multiplication_optimization/hdl/multiply_top.sv
+++ multiplication_optimization/hdl/multiply_top.sv
@@ -5,17 +5,83 @@ module multiply_top
     (
         input wire clk_i, reset_i,
         input wire [31:0] a_i, b_i,
-        output logic [3:0] product_o
+        output logic [31:0] product_o
     );
 
-    always_ff @(posedge clk_i) begin
+    logic sign_a;
+    logic [7:0] exponent_a;
+    logic [22:0] mantissa_a;
+
+    logic sign_b;
+    logic [7:0] exponent_b;
+    logic [22:0] mantissa_b;
+
+    logic [47:0] product_mantissa_reg, product_mantissa_next;
+    logic [31:0] product_reg, product_next;
+
+    typedef enum logic [1:0] { 
+        IDLE,
+        MULTIPLY,
+        NORMALIZE,
+        DONE
+    } state_t;
+
+    state_t state_reg, state_next;
+
+    assign sign_a = a_i[31];
+    assign exponent_a = a_i[30:23];
+    assign mantissa_a = a_i[22:0];
+
+    assign sign_b = b_i[31];
+    assign exponent_b = b_i[30:23];
+    assign mantissa_b = b_i[22:0];
+
+    always @(posedge clk_i) begin
         if (reset_i) begin
-            
+            state_reg = IDLE;
+            product_reg = 0;
+            product_mantissa_reg = 0;
         end else begin
-            
+            state_reg = state_next;
+            product_reg = product_next;
+            product_mantissa_reg = product_mantissa_next;
         end
     end
 
-    assign product = a * b;
+    always_comb begin
+        state_next = state_reg;
+        product_next = product_reg;
+        product_mantissa_next = product_mantissa_reg;
+        product_o = 0;
+        case (state_reg)
+            IDLE : begin
+                state_next = MULTIPLY;
+            end
+            MULTIPLY : begin
+                product_mantissa_next = {1'b1, mantissa_a} * {1'b1, mantissa_b};
+
+                product_next[31] = sign_a ^ sign_b;
+                product_next[30:23] = exponent_a + exponent_b - 8'd127;
+
+                state_next = NORMALIZE;
+            end
+            NORMALIZE : begin
+                if (product_mantissa_reg[47]) begin
+                    product_next[22:0] = {product_mantissa_reg[45:24] >> 1, 1'b0};
+                    product_next[30:23] = product_reg[30:23] + 1;
+                end else begin
+                    product_next[22:0] = {product_mantissa_reg[45:24], 1'b0};
+                end
+                state_next = DONE;
+            end
+            DONE : begin
+                product_o = product_reg;
+                state_next = DONE;
+            end
+            default : begin
+                state_next = IDLE;
+            end
+        endcase
+    end
 
 endmodule
--- multiplication_optimization/testbenches/multiply_top_tb.sv
+++ multiplication_optimization/testbenches/multiply_top_tb.sv
@@ -5,6 +5,8 @@ module multiply_top_tb;
 
     parameter CLK_PERIOD_NS = 10;
     logic clk_i, reset_i;
+    logic [31:0] a_i, b_i;
+    wire [31:0] product_o;
 
     multiply_top #(
     ) UUT(
@@ -20,7 +22,43 @@ module multiply_top_tb;
         reset_i = 1;
         repeat(1) @(negedge clk_i);
         reset_i = 0;
+        
+        // Test case 1: 2.5 * 4 = 10
+        a_i = 32'h40200000; // 2.5 in IEEE 754
+        b_i = 32'h40800000; // 4 in IEEE 754
+        repeat(3) @(negedge clk_i);
+        $display("Expected: 32'h41200000, Got: %h", product_o);
+
+        reset_i = 1;
+        repeat(1) @(negedge clk_i);
+        reset_i = 0;
+
+        // Test case 2: -3 * 7 = -21
+        a_i = 32'hC0400000; // -3 in IEEE 754
+        b_i = 32'h40E00000; // 7 in IEEE 754
+        repeat(3) @(negedge clk_i);
+        $display("Expected: 32'hC1A80000, Got: %h", product_o);
+
+        reset_i = 1;
+        repeat(1) @(negedge clk_i);
+        reset_i = 0;
+
+        // Test case 3: 0 * 1 = 0
+        a_i = 32'h00000000; // 0 in IEEE 754
+        b_i = 32'h3F800000; // 1 in IEEE 754
+        repeat(3) @(negedge clk_i);
+        $display("Expected: 32'h00000000, Got: %h", product_o);
+
+        reset_i = 1;
+        repeat(1) @(negedge clk_i);
+        reset_i = 0;
+
+        // Test case 4: 1.5 * 1.5 = 2.25
+        a_i = 32'h3FC00000; // 1.5 in IEEE 754
+        b_i = 32'h3FC00000; // 1.5 in IEEE 754
+        repeat(3) @(negedge clk_i);
+        $display("Expected: 32'h40100000, Got: %h", product_o);
+
         $finish;
     end
-
 endmodule
